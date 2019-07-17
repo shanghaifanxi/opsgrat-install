587d63e1eb803bc86d5f683715eb15c362e87147
jackson <jackson@shfanxi.com.address>
2019-07-17 10:21:49 +0800

    '代码提交'

--- install/install/urls_api.py
+++ install/install/urls_api.py
@@ -2,15 +2,15 @@ from django.conf.urls import url
 from install import apis
 
 urlpatterns = [
-    url('setup/', apis.setup.as_view(), name='api_setup'),
+    url('setup/', apis.setup, name='api_setup'),
 
-    url('check/', apis.check.as_view(), name='api_check'),
+    url('check/', apis.check, name='api_check'),
 
-    url('setupAnsible/', apis.setupAnsible.as_view(), name='api_setup_ansible'),
+    url('setupAnsible/', apis.setupAnsible, name='api_setup_ansible'),
 
-    url('setupPip/', apis.setupPip.as_view(), name='api_setup_pip'),
+    url('setupPip/', apis.setupPip, name='api_setup_pip'),
 
-    url('setupSshPass/', apis.setupSshPass.as_view(), name='api_setup_sshpass'),
+    url('setupSshPass/', apis.setupSshPass, name='api_setup_sshpass'),
 
-    url('stopPid/', apis.stopPid.as_view(), name='api_stop_pid'),
+    url('stopPid/', apis.stopPid, name='api_stop_pid'),
     ]
