0d427a19a342b290983afdce54f6c0f9704b9b04
陈 绍东 <chenshaodong@chendeMacBook-Pro.local>
2019-07-18 15:02:50 +0800

    安装gcc，禁用selinux

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-yum install python-devel
+yum install python-devel -y
 if [ `command -v pip` ];then
     echo 'pip 已经安装'
 else
     wget https://bootstrap.pypa.io/get-pip.py
     python get-pip.py
+fi
+if [ `command -v gcc` ];then
+    echo 'gcc 已经安装'
+else
+    yum install gcc -y
 fi
 pip install ansible==2.8.2
 yum install sshpass -y
