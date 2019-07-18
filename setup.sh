f41a39cb3bc5daa542e693c36b548e0b10b9a04f
陈 绍东 <chenshaodong@chendeMacBook-Pro.local>
2019-07-18 08:51:59 +0800

    安装脚本增加pip安装

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+yum install python-devel
+if [ `command -v pip` ];then
+    echo 'pip 已经安装'
+else
+    wget https://bootstrap.pypa.io/get-pip.py
+    python get-pip.py
+fi
 cd install
 pip install -r req.txt
 python manage.py migrate
